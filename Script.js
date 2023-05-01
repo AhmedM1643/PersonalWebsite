f911dab92a76aea0ba951bc3d8829a03c6dffa6d
AhmedM1643 <106673577+AhmedM1643@users.noreply.github.com>
2023-05-01 09:38:16 +0200

    Update Script.js

--- Script.js
+++ Script.js
@@ -17,3 +17,13 @@ window.addEventListener('scroll', reveal);
         }
       }
     }
+
+const Scroll = ScrollReveal({
+  origin: 'top',
+  distance: '60px',
+  duration: 1500,
+  delay: 200,
+//     reset: true
+});
+
+Scroll.reveal('.Main,.section-title,.AboutMe,.Skills,.Skill,.Exp,.Services,.GridLayout,.Service,.Projects,.Project,.Contact,.ContactForm,Input,textarea',{}); 
